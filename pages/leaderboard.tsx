aac0eb84418f38d5c20823551af7dea2bb8d99c1
tbtstl <tysonbattistella@gmail.com>
2023-08-22 16:00:03 -0700

    [feat] time remaining

--- pages/leaderboard.tsx
+++ pages/leaderboard.tsx
@@ -8,13 +8,48 @@ interface Leader {
     score: number;
 }
 
+function timeUntil(start, end) {
+    const timestampInMilliseconds = end;
+    let difference = end - start;
+
+    if (difference <= 0) {
+        return "Game Over";
+    }
+
+    const days = Math.floor(difference / (24 * 60 * 60));
+    difference -= days * (24 * 60 * 60);
+
+    const hours = Math.floor(difference / (60 * 60));
+    difference -= hours * (60 * 60);
+
+    const minutes = Math.floor(difference / (60));
+    difference -= minutes * (60);
+
+    const seconds = Math.floor(difference);
+
+    let result = "";
+    if (days > 0) result += days + " days, ";
+    if (hours > 0) result += hours + " hours, ";
+    if (minutes > 0) result += minutes + " minutes, ";
+    result += "and " + seconds + " seconds";
+
+    return result;
+}
+
+
 export default function Leaderboard({ leaders }) {
-    console.log({ leaders })
+    const deadline = 1697920553;
+    const now = Math.floor(Date.now() / 1000);
+    const secondsLeft = deadline - now;
+    const blocksLeft = Math.floor(secondsLeft / 2);
     return (
         <Container>
             {leaders.map((leader, idx) => (
-                <LeaderLine leader={leader} idx={idx} />
+                <LeaderLine leader={leader} idx={idx} key={leader.id} />
             ))}
+            <br />
+            <p>There are <b>~{blocksLeft} blocks</b> left to claim before the deadline</p>
+            <p>The game will be over in {timeUntil(now, deadline)}</p>
         </Container>
     )
 }
